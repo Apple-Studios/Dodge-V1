7ecf5ac742cb8619f02c48a5705226e4b5b5798a
Ben Parrott <benparrott@mmc.school.nz>
2019-06-20 11:43:53 +1200

    Add project files.

---
+++ Dodge V1/frmDodge.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Dodge_V1
+{
+    partial class frmDodge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlGame = new System.Windows.Forms.Panel();
+            this.tmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.tmrShip = new System.Windows.Forms.Timer(this.components);
+            this.Name = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Planet Dodge";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.BackColor = System.Drawing.Color.PaleGreen;
+            this.pnlGame.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlGame.Location = new System.Drawing.Point(12, 49);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(500, 400);
+            this.pnlGame.TabIndex = 1;
+            this.pnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlGame_Paint);
+            // 
+            // tmrPlanet
+            // 
+            this.tmrPlanet.Enabled = true;
+            this.tmrPlanet.Tick += new System.EventHandler(this.tmrPlanet_Tick);
+            // 
+            // tmrShip
+            // 
+            this.tmrShip.Enabled = true;
+            this.tmrShip.Interval = 50;
+            this.tmrShip.Tick += new System.EventHandler(this.tmrShip_Tick);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(553, 109);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(35, 13);
+            this.Name.TabIndex = 2;
+            this.Name.Text = "Name";
+            this.Name.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(522, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Score";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(553, 216);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "label3";
+            this.lblScore.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // frmDodge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.pnlGame);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "frmDodge";
+            this.Text = "Dodge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmDodge_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmDodge_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlGame;
+        private System.Windows.Forms.Timer tmrPlanet;
+        private System.Windows.Forms.Timer tmrShip;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
